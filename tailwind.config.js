7b1d9694228bf9e772f52fa86ece1dab2dc85525
Daniel Mombeyni <mombeyni.daniel@gmail.com>
2024-06-09 16:03:29 +0330

    add Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
-  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        roboto: ["Roboto"],
+      },
+    },
   },
   plugins: [],
 };
